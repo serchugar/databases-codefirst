ce974617dfbde9ccc89cf866c1c2cbd85dd9d4d6
chugarsergio <chugarsergio@gmail.com>
2025-06-23 11:40:47 +0200

    feat(shared): add Tournament entity and TeamTournament joined table entity for N:N relationship

---
+++ Shared/Entities/Joins/TeamTournament.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Shared.ValueObjects;
+
+namespace Shared.Entities.Joins;
+
+[Table("team_tournaments", Schema = "code_first")]
+public class TeamTournament
+{
+    [Column("team_id"), Key]
+    public int TeamId { get; set; }
+    
+    [Column("tournament_id"), Key]
+    public int TournamentId { get; set; }
+
+    // Owned tables
+    public AuditInfo AuditInfo { get; set; } = new();
+    
+    // Nav props
+    public Team Team { get; set; } = null!;
+    public Tournament Tournament { get; set; } = null!;
+}
--- Shared/Entities/Tournament.cs
+++ Shared/Entities/Tournament.cs
@@ -5,28 +5,21 @@ using Shared.ValueObjects;
 
 namespace Shared.Entities;
 
-[Table("teams", Schema = "code_first")]
-public class Team
+[Table("tournaments", Schema = "code_first")]
+public class Tournament
 {
     [Column("id"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
-    [Column("name"), Required, StringLength(64)]
+    
+    [Column("name"), Required, StringLength(128)]
     public string Name { get; set; } = null!;
-    
-    [Column("country_id"), Required]
-    public int CountryId { get; set; }
-    
     
     // Owned tables
     public AuditInfo AuditInfo { get; set; } = new();
     
     // Nav props
-    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
-    public virtual Country Country { get; set; } = null!;
     public virtual ICollection<TeamTournament> TeamTournaments { get; set; } = new List<TeamTournament>();
     
     // Readonly props
-    public int PlayerCount => Players.Count;
-    public int TournamentCount => TeamTournaments.Count;
+    public int TeamsCount => TeamTournaments.Count;
 }
