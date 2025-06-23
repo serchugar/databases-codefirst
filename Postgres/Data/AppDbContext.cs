b3839a18871411d9e46d35a531aa9b7d90a17f59
chugarsergio <chugarsergio@gmail.com>
2025-06-23 11:36:47 +0200

    fix(postres): add Countries, Teams, Players DbSet to AppDbContext

--- Postgres/Data/AppDbContext.cs
+++ Postgres/Data/AppDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Entities;
 
 namespace Postgres.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Player> Players { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
