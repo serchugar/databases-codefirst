96030d3e1c8e7082f7ef584d6fe47f0904145f30
chugarsergio <chugarsergio@gmail.com>
2025-06-23 11:09:44 +0200

    feat(postgres): add AppDbContext.cs and Npgsql DI

---
+++ Postgres/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Postgres.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
--- Postgres/Program.cs
+++ Postgres/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.EntityFrameworkCore;
 using Postgres.Configuration;
+using Postgres.Data;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+if (builder.Environment.IsDevelopment()) builder.Configuration.AddUserSecrets<Program>();
 #region Services
+builder.Services.AddDbContextPool<AppDbContext>(opts => 
+    opts.UseNpgsql(
+        builder.Configuration.GetConnectionString("testing"),
+        o => o.MigrationsHistoryTable("__EFMigrationHistory", "code_first")));
 builder.Services.AddDependencyInjectionConfig();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerConfig();
+if(builder.Environment.IsDevelopment()) builder.Services.AddSwaggerConfig();
 #endregion
 
 WebApplication app = builder.Build();
 #region Middleware
-app.UseSwaggerConfig();
+if(app.Environment.IsDevelopment()) app.UseSwaggerConfig();
 #endregion
 
 app.MapControllers();
